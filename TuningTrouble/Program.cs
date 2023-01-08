debf8fabacd015561e259027e03a7186647aec27
valentincpopa <38735469+valentincpopa@users.noreply.github.com>
2023-01-09 00:55:38 +0200

    day 6

---
+++ TuningTrouble/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TuningTrouble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Resolve(4);
+            Resolve(14);
+        }
+
+        static void Resolve(int noOfDistinctCharacters)
+        {
+            var signalMessage = File.ReadAllText("./input.txt");
+
+            var charIndex = 0;
+            var marker = new Queue<char>();
+
+            while (true)
+            {
+                if (marker.Count > noOfDistinctCharacters)
+                {
+                    marker.Dequeue();
+                }
+
+                if (marker.Count == noOfDistinctCharacters && marker.Distinct().Count() == noOfDistinctCharacters)
+                {
+                    Console.WriteLine(charIndex);
+                    break;
+                }
+
+                var currentChar = signalMessage[charIndex];
+                marker.Enqueue(currentChar);
+
+                charIndex++;
+            }
+        }
+    }
+}
