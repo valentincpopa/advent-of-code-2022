833593789d405caf63b282d18859f5dd0f583fc1
valentincpopa <38735469+valentincpopa@users.noreply.github.com>
2023-01-18 21:05:32 +0200

    day 11

---
+++ MonkeyInTheMiddle/Monkey.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MonkeyInTheMiddle
+{
+    public class Monkey
+    {
+        public int Identifier { get; set; }
+        public Queue<Item> Items { get; set; }
+        public double InspectionsCount { get; set; }
+        public List<string> OperationTokens { get; set; }
+        public int TestCondition { get; set; }
+        public Dictionary<string, int> PossibleOutcomes { get; set; }
+
+        public Monkey()
+        {
+            Items = new Queue<Item>();
+            OperationTokens = new List<string>();
+            PossibleOutcomes = new Dictionary<string, int>();
+        }
+
+        public void ParseInput(string[] input)
+        {
+            ParseIdentifier(input[0]);
+            ParseItems(input[1]);
+            ParseOperationTokens(input[2]);
+            ParseTestCondition(input[3]);
+            ParsePossibleOutcome(input[4]);
+            ParsePossibleOutcome(input[5]);
+        }
+
+        private void ParseIdentifier(string rawIdentifierRow)
+        {
+            var regex = new Regex("Monkey ([0-9]+)");
+            var matchGroups = regex.Match(rawIdentifierRow).Groups.Values.ToList();
+
+            var monkeyIdentifier = int.Parse(matchGroups[1].Value);
+            Identifier = monkeyIdentifier;
+        }
+
+        private void ParseItems(string rawItemsRow)
+        {
+            var regex = new Regex("([0-9]+)");
+            var matches = regex.Matches(rawItemsRow).ToList();
+
+            for (int i = 0; i < matches.Count; i++)
+            {
+                Items.Enqueue(new Item(int.Parse(matches[i].Value)));
+            }
+        }
+
+        private void ParseOperationTokens(string rawOperationsRow)
+        {
+            var regex = new Regex("= (.*)");
+            var matchGroups = regex.Match(rawOperationsRow).Groups.Values.ToList();
+
+            OperationTokens.AddRange(matchGroups[1].Value.Split(" ", StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        private void ParseTestCondition(string rawTestConditionRow)
+        {
+            var regex = new Regex("by ([0-9]+)");
+            var matchGroups = regex.Match(rawTestConditionRow).Groups.Values.ToList();
+
+            TestCondition = int.Parse(matchGroups[1].Value);
+        }
+
+        private void ParsePossibleOutcome(string rawPossibleOutcomeRow)
+        {
+            var regex = new Regex("If (.*): throw to monkey ([0-9]+)");
+            var matchGroups = regex.Match(rawPossibleOutcomeRow).Groups.Values.ToList();
+
+            PossibleOutcomes.Add(matchGroups[1].Value, int.Parse(matchGroups[2].Value));
+        }
+
+        public Queue<(Item item, int monkeyIdentifier)> ProcessItems(bool reliefIsModulo, int reliefValue)
+        {
+            var processedItems = new Queue<(Item item, int monkeyIdentifier)>();
+
+            while (Items.Any())
+            {
+                var item = Items.Dequeue();
+                ProcessOperation(item);
+                ProcessRelief(item, reliefIsModulo, reliefValue);
+
+                var testOutcome = RetrieveTestOutcome(item);
+
+                processedItems.Enqueue((item, testOutcome));
+
+                InspectionsCount++;
+            }
+
+            return processedItems;
+        }
+
+        private void ProcessOperation(Item item)
+        {
+            var firstOperand = OperationTokens[0] == "old" ? item.WorryLevel : double.Parse(OperationTokens[0]);
+            var secondOperand = OperationTokens[2] == "old" ? item.WorryLevel : double.Parse(OperationTokens[2]);
+            var operation = GetOperation(OperationTokens[1]);
+
+            item.WorryLevel = operation(firstOperand, secondOperand);
+        }
+
+        private Func<double, double, double> GetOperation(string operation)
+        {
+            return operation switch
+            {
+                "*" => (a, b) => { return a * b; },
+                "+" => (a, b) => { return a + b; }
+            };
+        }
+
+        private void ProcessRelief(Item item, bool reliefIsModulo, int reliefValue)
+        {
+            if (!reliefIsModulo)
+            {
+                item.WorryLevel = (int)Math.Floor((decimal)(item.WorryLevel / reliefValue));
+            }
+            else
+            {
+                item.WorryLevel = item.WorryLevel % reliefValue;
+            }
+        }
+
+        private int RetrieveTestOutcome(Item item)
+        {
+            var isDivisible = item.WorryLevel % TestCondition == 0;
+            return PossibleOutcomes[isDivisible.ToString().ToLower()];
+        }
+    }
+}
---
+++ MonkeyInTheMiddle/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MonkeyInTheMiddle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rawNotes = File.ReadAllLines("./input.txt")
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToList();
+
+            Resolve(20, rawNotes, false);
+            Resolve(10000, rawNotes, true);
+        }
+
+        static void Resolve(int rounds, List<string> rawNotes, bool reliefIsModulo)
+        {
+            var monkeys = new List<Monkey>();
+            var monkeysRawNotes = rawNotes.Chunk(6).ToList();
+
+            foreach (var monkeyRawNotes in monkeysRawNotes)
+            {
+                var monkey = new Monkey();
+                monkey.ParseInput(monkeyRawNotes);
+                monkeys.Add(monkey);
+            }
+
+            var leastCommonMultiple = monkeys.Select(x => x.TestCondition).Aggregate((x, y) => x * y);
+
+            for (int i = 0; i < rounds; i++)
+            {
+
+                foreach (var monkey in monkeys)
+                {
+                    var processedItems = monkey.ProcessItems(reliefIsModulo, reliefIsModulo ? leastCommonMultiple : 3);
+
+                    while (processedItems.Any())
+                    {
+                        (var item, var monkeyIdentifier) = processedItems.Dequeue();
+                        var monkeyToReceiveItem = monkeys.First(x => x.Identifier == monkeyIdentifier);
+                        monkeyToReceiveItem.Items.Enqueue(item);
+                    }
+                }
+            }
+
+            var mostActiveMonkeys = monkeys.OrderByDescending(x => x.InspectionsCount).Take(2).ToList();
+
+            Console.WriteLine(mostActiveMonkeys[0].InspectionsCount * mostActiveMonkeys[1].InspectionsCount);
+        }
+    }
+}
